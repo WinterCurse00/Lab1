d4fbdfa246ee5a402a78ed1215ba1c3e62078775
Daniel <danielagachi@gmail.com>
2023-03-30 15:47:12 +0300

    lab4 commit update

--- src/lab4inter.ts
+++ src/lab4inter.ts
@@ -1,22 +1,22 @@
-interface person {
+interface Person {
     name: string;
     age: number;
     email: string;
     address: string;
     phone: string;
 }
-interface personjob extends person {
-    occupation: string;
+interface PersonJob extends Person {
+    profession: string;
     salary: number;
 }
 
-const user: personjob = {
+const user: PersonJob = {
     name: "Agachi Daniel",
     age: 22,
     email: "daniel.agachi@iis.utm.md",
     address: "Independentii 4",
     phone: "0896534223",
-    occupation: "Engineer",
+    profession: "Engineer",
     salary: 5000
 };
 
@@ -25,5 +25,5 @@ console.log(user.age);
 console.log(user.email);
 console.log(user.address);
 console.log(user.phone);
-console.log(user.occupation);
+console.log(user.profession);
 console.log(user.salary + "$");
